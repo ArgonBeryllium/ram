a6da550e6b4c61337958f6e5601716e942522a83
ArBe <argon-beryllium@protonmail.com>
2021-06-15 12:29:54 +0200

    added GI, decreased light intensity

--- src/globals.h
+++ src/globals.h
@@ -5,6 +5,7 @@
 constexpr inline int RW = 540, RH = 480, PS=5,
 						W = RW/PS, H = RH/PS;
 inline v3f lp = {3, 1, -4};
-inline float li = 110;
+inline float li = 20;
 struct SDF;
 inline std::vector<SDF*> world;
+inline const SDL_Colour GI = {50,50,50,255};
