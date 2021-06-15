de27082742c35c6746f13ba25cc6d50a41fdc9ad
ArBe <argon-beryllium@protonmail.com>
2021-06-15 15:04:35 +0200

    changed fov

--- src/camera.cpp
+++ src/camera.cpp
@@ -3,11 +3,9 @@
 
 v3f Camera::pos = {0,0,-5};
 v3f Camera::angles = {};
-float Camera::fov = M_PI_2f32;
-float Camera::plane_offset = 1;
-void Camera::init()
-{
-};
+float Camera::fov = M_PIf32/4;
+float Camera::plane_offset = .2;
+void Camera::init() {};
 
 static v2f getPlaneDims()
 {
@@ -23,4 +21,4 @@ v3f Camera::getRayDir(const v2i &pp)
 	float xs = float(pp.x*2 - W)/W;
 	float ys = float(pp.y*2 - H)/H;
 	return v3f{std::cos(fov/2*xs - M_PI_2f32), -std::cos(fov/2*ys - M_PI_2f32),std::min(1.f, std::sin(fov/2*ys - M_PI_2f32)*std::sin(fov/2*xs - M_PI_2f32))};
-};
+}
